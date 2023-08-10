302fe0805bf791aa35c6898154af966a0254b0b0
Naveen Nagarajan <naveen@pmsquaresoft.com>
2023-08-10 10:15:04 +0530

    clear console

--- server.js
+++ server.js
@@ -13,23 +13,10 @@ connection.connect((err) => {
   console.log("Mysql database connected successfully!!!");
 });
 
-// connection.query("SELECT * FROM todolist", (err, res) => {
-//   if (err) console.log(`DB Error: ${err}`);
-//   console.log(`Results Todos: ${res}`);
-// });
-
-// const sqlQuery = "SELECT * FROM todolist";
-// const todos = connection.query(sqlQuery, (err, res) => {
-//   if (err) throw err;
-//   console.log(res);
-// });
-// console.log(todos);
-
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is Running at 127.0.0.1:${port}`);
 });
 
-// console.log(`Server: ${con}`);
 module.exports = connection;
